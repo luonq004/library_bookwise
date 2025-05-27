eafb3bff18b41f55a92176ad96cdcc8fcdb9cc22
Luong <luongvq004@gmail.com>
2025-05-27 23:17:57 +0700

    setup workflow

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  typescript: {
+    ignoreBuildErrors: true, // This is not recommended for production, but can be useful during development
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // This is not recommended for production, but can be useful during development
+  },
 };
 
 export default nextConfig;
